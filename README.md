47213f02c4da774251ba5f7a7d056fdcddce7515
Yasuaki Kinjo <mail@4-ner.com>
2017-07-09 11:37:46 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# middleman-bunbun
+middleman-bunbun is a boilerplate of middleman using using Haml
+
+## Feature
+- Live reloadでブラウザ自動リロード
+- Haml + Sass + ES6 ですぐ開発スタート
+
+## Front End
+- jQuery
+- SCSS(twitter bootstrap v4.0.0-alpha.6)
+- Autoprefixer
+- Babel
+- AirBnB ESlint
+- Webpack (for JS module bundling)
+
+## Setup
+
+    $ yarn install
+    $ bundle install --path vendor/bundle
+
+
+# :tada: Are you ready to the party :tada:
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var Clean = require('clean-webpack-plugin');
+module.exports = {
+  entry: {
+    site: [
+      './source/stylesheets/site.css.scss',
+      './source/javascripts/site.js'
+    ],
+  },
+  resolve: {
+    modules: [
+      __dirname + '/assets/javascripts',
+      __dirname + '/assets/stylesheets',
+      __dirname + '/node_modules',
+    ],
+    extensions: ['.js', '.css', '.scss']
+  },
+  output: {
+    path: __dirname + '/.tmp/dist',
+    filename: 'javascripts/[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /source\/javascripts\/.*\.js$/,
+        exclude: /node_modules|\.tmp|vendor/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$|.sass$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        }),
+      }
+    ]
+  },
+  plugins: [
+    // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
+    // inside your code for any environment checks; UglifyJS will automatically
+    // drop any unreachable code.
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new Clean(['.tmp']),
+    new ExtractTextPlugin("stylesheets/[name].css"),
+  ],
+};
